038893700f2e5b9db7fd15cbf039c582dedd7b8d
Steven Huang <steven.charles.huang@gmail.com>
2019-10-01 22:51:15 -0400

    have most basic question working for superman

--- src/com/ga/superhero/Superhero.java
+++ src/com/ga/superhero/Superhero.java
@@ -3,23 +3,32 @@ package com.ga.superhero;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Superhero {
+    private static int correct = 0;
+    private static int incorrect = 0;
+    private static File input = new File("./input.txt");
+    private Scanner sc = new Scanner(input);
+    private static ArrayList<String> trivia = new ArrayList<>();
 
     public Superhero() throws FileNotFoundException {
-        int correct = 0;
-        int incorrect = 0;
+        String username = "";
 
-        System.out.println("Hello Superheroes");
+
         // https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
-        File input = new File("./input.txt");
-        Scanner sc = new Scanner(input);
 
-//        while (sc.hasNextLine()) System.out.println(sc.nextLine());
+        while (sc.hasNextLine()) {
+            trivia.add(sc.nextLine());
+        }
+
+        System.out.println(trivia);
+
+        username = getInput("Enter your name");
 
         // https://git.generalassemb.ly/nyc-s1-1/foundational-java/blob/master/java-basics/debugging-lesson/starter-code/simple-calculator/src/com/ga/Main.java
-        System.out.println("Choose a superhero: \n1. superman \n2. batman \n3. spiderman \n4. wonder woman\n");
+        System.out.println("Choose a superhero: \n1. superman \n2. batman \n3. spiderman \n4. wonder woman");
 
         Scanner scanner = new Scanner(System.in);
 
@@ -37,19 +46,24 @@ public class Superhero {
             System.out.println("Invalid input!");
         }
 
-        outputResults(correct, incorrect);
+        outputResults(username, correct, incorrect);
     }
 
     // https://git.generalassemb.ly/nyc-s1-1/foundational-java/blob/master/java-basics/debugging-lesson/starter-code/simple-calculator/src/com/ga/Main.java
     public static String getInput(String context){
-        System.out.print(context+": ");
+        System.out.print(context + ": ");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
     public static void superman() {
-        String response = "superman";
-        System.out.println(response);
+        String a1 = getInput("What city does this superhero defend?");
+
+        if (a1.equalsIgnoreCase(trivia.get(trivia.indexOf("Superman") + 1))) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("Incorrect...");
+        }
     }
 
     public static void batman() {
@@ -67,12 +81,12 @@ public class Superhero {
         System.out.println(response);
     }
 
-    public static void outputResults(int right, int wrong) throws FileNotFoundException {
+    public static void outputResults(String name, int right, int wrong) throws FileNotFoundException {
         String result = (right >= wrong) ? "You won! =) Play again!" : "You lost! =( Play again!";
         // https://www.geeksforgeeks.org/redirecting-system-out-println-output-to-a-file-in-java/
         File output = new File("./output.txt");
         PrintStream out = new PrintStream(output);
         System.setOut(out);
-        System.out.println(result);
+        System.out.println("Hey, " + name + "! \n\n" + result + "\n\nYou got this many answers right: " + right + "\nYou got this many answers wrong: " + wrong);
     }
 }
