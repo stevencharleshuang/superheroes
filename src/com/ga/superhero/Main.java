c09584f8bc83758f1642fd9d43552ff922dcaed7
Steven Huang <steven.charles.huang@gmail.com>
2019-10-01 21:18:53 -0400

    reading from input.txt and getting some user input

--- src/com/ga/superhero/Main.java
+++ src/com/ga/superhero/Main.java
@@ -1,10 +1,14 @@
 package com.ga.superhero;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-
+		new Superhero();
 	}
-	
+
 }
--- src/com/ga/superhero/Superhero.java
+++ src/com/ga/superhero/Superhero.java
@@ -1,5 +1,68 @@
 package com.ga.superhero;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Superhero {
 
+    public Superhero() throws FileNotFoundException {
+        int correct = 0;
+        int incorrect = 0;
+
+        System.out.println("Hello Superheroes");
+        // https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
+        File file =
+                new File("./input.txt");
+        Scanner sc = new Scanner(file);
+
+//        while (sc.hasNextLine())
+//            System.out.println(sc.nextLine());
+
+        // https://git.generalassemb.ly/nyc-s1-1/foundational-java/blob/master/java-basics/debugging-lesson/starter-code/simple-calculator/src/com/ga/Main.java
+        System.out.println("Choose a superhero: \n1. superman \n2. batman \n3. spiderman \n4. wonder woman\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String answer = scanner.nextLine();
+
+        if(answer.equals("1") || answer.equals("superman")){
+            superman();
+        } else if(answer.equals("2") || answer.equals("batman")){
+            batman();
+        } else if(answer.equals("3") || answer.equals("spiderman")){
+            spiderman();
+        } else if(answer.equals("4") || answer.equals("wonder woman")){
+            wonderWoman();
+        } else {
+            System.out.println("Invalid input!");
+        }
+    }
+
+    // https://git.generalassemb.ly/nyc-s1-1/foundational-java/blob/master/java-basics/debugging-lesson/starter-code/simple-calculator/src/com/ga/Main.java
+    public static String getInput(String context){
+        System.out.print(context+": ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public static void superman() {
+        String response = "superman";
+        System.out.println(response);
+    }
+
+    public static void batman() {
+        String response = "batman";
+        System.out.println(response);
+    }
+
+    public static void spiderman() {
+        String response = "spiderman";
+        System.out.println(response);
+    }
+
+    public static void wonderWoman() {
+        String response = "wonder woman";
+        System.out.println(response);
+    }
 }
